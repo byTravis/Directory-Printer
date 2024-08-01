0680d6959f132319e067f33a33ca7185eae53ee3
Travis Cook <justuscooks@gmail.com>
2024-08-01 15:32:20 -0700

    cleaned up code

--- DirectoryPrinter.py
+++ DirectoryPrinter.py
@@ -7,7 +7,7 @@ import re
 
 # Set up environment
 root = ctk.CTk()
-root.title("Trav's Directory Printer")
+root.title("Directory Printer - byTravis")
 
 # Window size and position
 window_width = 600
@@ -27,9 +27,10 @@ root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
 cur_dir = os.getcwd()
 
 
-
-
+#################################################
 # Functions
+#################################################
+
 
 def open_dir():
 	global cur_dir
@@ -46,10 +47,10 @@ def get_file_list():
 	file_types_list = get_file_types()
 	for file in os.listdir(cur_dir):
 		file_path = os.path.join(cur_dir, file)
+
 		if os.path.isfile(file_path):
-            # Get the file extension
 			_, ext = os.path.splitext(file)
-			ext = ext[1:].lower()  # Remove the dot and convert to lowercase
+			ext = ext[1:].lower()
             
 			if not file_types_list or ext in file_types_list:
 				file_list.append(file)
@@ -58,9 +59,7 @@ def get_file_list():
 			dir = f"[{file}]"
 			dir_list.append(dir)
 
-
 	list_files(file_list, dir_list)
-
 
 
 
@@ -83,7 +82,7 @@ def list_files(file_list, dir_list):
 	file_text.insert(ctk.END, list_files)
 
 	
-		
+
 def save_txt():
 	global cur_dir
 
@@ -98,6 +97,7 @@ def save_txt():
 		with open(file_path, 'w') as file:
 			file.write(file_text.get("1.0", ctk.END))
 
+
 def get_file_types():
 	file_types_str = file_types.get()
 	file_types_str = file_types_str.replace('.', '').replace('*', '')
@@ -105,7 +105,6 @@ def get_file_types():
 	file_types_list = [file_type.strip() for file_type in re.split(r'[,\s]+', file_types_str) if file_type]
 
 	return file_types_list
-
 
 
 
@@ -129,8 +128,7 @@ btn_highlight_color = "#96d3e2"
 btn_highlight_text_color = "black"
 
 
-# Styling
-
+# ===  Styling  ===  
 cb_width = 20
 cb_height = 20
 cb_radius = 10
@@ -150,22 +148,16 @@ column1_container.grid(column=1, row=0, padx=10, pady=10, sticky="nsew")
 
 # Configure grid weights for the root to allow columns to resize
 root.grid_columnconfigure(0, weight=1)
-# root.grid_columnconfigure(1, weight=1)
 root.grid_rowconfigure(0, weight=1)
 
 # Configure grid weights for column0_container to allow the text box to resize
 column0_container.grid_columnconfigure(0, weight=1)
 column0_container.grid_rowconfigure(0, weight=1)
 
-# column1_container.grid_columnconfigure(0, weight=1)
-# column1_container.grid_rowconfigure(0, weight=1)
-
-
 
 # Text Box
 file_text = ctk.CTkTextbox(column0_container)
 file_text.grid(column=0, row=0, padx=10, pady=10, sticky="nsew")
-
 
 
 # Options
@@ -179,12 +171,12 @@ label = ctk.CTkLabel(options_container, text="Options", font=section_title)
 label.grid(column=0, row=0, sticky="w", padx=5, pady=10)
 
 include_path_status = ctk.StringVar(value="off")
-include_path = ctk.CTkCheckBox(options_container, text="Include file path?", variable=include_path_status, onvalue="on", offvalue="off",
+include_path = ctk.CTkCheckBox(options_container, text="Include folder path?", variable=include_path_status, onvalue="on", offvalue="off",
 							   border_width=cb_border, checkbox_height=cb_height, checkbox_width=cb_width, corner_radius=cb_radius)
 include_path.grid(column=0, row=5, padx=(15,5), pady=0, sticky=W)
 
 include_directories_status = ctk.StringVar(value="off")
-include_directories = ctk.CTkCheckBox(options_container, text="Include directories?", variable=include_directories_status, onvalue="on", offvalue="off",
+include_directories = ctk.CTkCheckBox(options_container, text="Include folders?", variable=include_directories_status, onvalue="on", offvalue="off",
 									border_width=cb_border, checkbox_height=cb_height, checkbox_width=cb_width, corner_radius=cb_radius)
 include_directories.grid(column=0, row=10, padx=(15,5), pady=(0,15), sticky=W)
 
@@ -197,7 +189,7 @@ file_types.grid (column=0, row=2, padx=(15,5), pady=(0,15), sticky=W)
 btn_container = ctk.CTkFrame(column1_container)
 btn_container.grid(column=0, row=1, pady=5)
 
-open_dir_btn = ctk.CTkButton(btn_container, text="Open Directory", command=open_dir)
+open_dir_btn = ctk.CTkButton(btn_container, text="Open Folder", command=open_dir)
 open_dir_btn.grid(column=0, row=0,pady=5)
 
 refresh_btn = ctk.CTkButton(btn_container, text="Refresh", command=get_file_list)
@@ -205,7 +197,6 @@ refresh_btn.grid(column=0, row=1, pady=5)
 
 save_txt_btn = ctk.CTkButton(btn_container, text="Save txt File", command=save_txt)
 save_txt_btn.grid(column=0, row=2, pady=5)
-
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Directory-Printer
-A simple that saves a list of files within a directory.
+*Easily list files in a folder*
+
+![Directory Printer](images/project_banner.jpg)
+
+## Overview
+This is a simple directory printer that makes it easy to list files within a folder so you can copy/paste into an email or document.  You can also save the list as a text file.  It includes a few options such as listing only specific filetype, include the directory path, and whether or not to include folders in your list.  
+
+![Directory Printer - Screenshot](images/screengrab.JPG)
+
+## How To Use
+
+**Open Folder** - This will allow you to browse to the folder you want to generate a list of files from.  Once you select a folder, it will list all the files within that directory.  It will also update the title bar with the folder's path.
+
+**Refresh** - This will update the file list.  This is useful if you change any options, or add additional files to your folder.
+
+**Save txt File** - This will save your file list to a txt file.  When you click, it will ask you where to save your file.  Give it a name and click Save.
+
+### Options
+
+**All File Types** - You can specify which files you are interested in by listing the extension.  You can list multiple file extensions by separating them with a comma or a space.  Click the Refresh button to update your list that includes only the files you are interested in.  To remove the filter, simply delete the extensions from this section.
+
+**Include folder path** - This will include the folder path at the top of the document.  This is nice for reference in case you need to go back.
+
+**Include folders** - In addition to the files, this option will also include the folder names in this directory.  Folders are specified with a square bracket around the name (ie [Folder Name]).  Click the Refresh button to update your list.
+
+
+## Libraries Used
+- [Tkinter](https://docs.python.org/3/library/tkinter.html)
+- [CustomTkinter](https://customtkinter.tomschimansky.com/)
+- [os](https://docs.python.org/3/library/os.html) (Operating System Interfaces)
+- [re](https://docs.python.org/3/library/re.html) (Regular Expressions)
+
